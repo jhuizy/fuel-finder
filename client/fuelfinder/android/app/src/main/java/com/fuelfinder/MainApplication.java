de882b151f0791c4e93b269a71d818582c0255b7
Jordan Huizenga <jhuizenga99@gmail.com>
2017-06-03 00:33:06 +0800

    adds basic pull list

--- client/fuelfinder/android/app/src/main/java/com/fuelfinder/MainApplication.java
+++ client/fuelfinder/android/app/src/main/java/com/fuelfinder/MainApplication.java
@@ -3,8 +3,8 @@ package com.fuelfinder;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.wix.interactable.Interactable;
 import com.airbnb.android.react.maps.MapsPackage;
+import com.wix.interactable.Interactable;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,8 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new Interactable(),
-            new MapsPackage()
+            new MapsPackage(),
+            new Interactable()
       );
     }
   };
--- client/fuelfinder/components/Home.js
+++ client/fuelfinder/components/Home.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 import React, { Component } from "react";
-import { StyleSheet, View, Dimensions } from "react-native";
+import { StyleSheet, View, Text, Dimensions } from "react-native";
 import Interactable from "react-native-interactable";
 import Map from "./Map";
 import List from "./List";
@@ -9,7 +9,7 @@ import MOCK_DATA from "../mock/mockdata";
 
 const Screen = {
   width: Dimensions.get("window").width,
-  height: Dimensions.get("window").height - 75
+  height: Dimensions.get("window").height
 };
 
 const performSearchMock = ({ latitude, longitude }) => {
@@ -50,13 +50,22 @@ export default class Home extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.background} />
+        <View style={styles.background}>
+          <Map markers={this.state.markers} onRegionChange={region => {}} />
+        </View>
         <Interactable.View
+          style={styles.interactable}
           verticalOnly={true}
-          snapPoints={[{ y: 40 }, { y: Screen.height - 70 }]}
-          boundaries={{ top: -300 }}
-          initialPosition={{ y: Screen.height - 70 }}
+          snapPoints={[
+            { y: 0 },
+            { y: Screen.height * 0.4 },
+            { y: Screen.height - 90 }
+          ]}
+          initialPosition={{ y: Screen.height - 90 }}
         >
+          <View style={styles.listHandle}>
+            <Text>{this.state.markers.length} result(s)</Text>
+          </View>
           <List style={styles.list} items={this.state.markers} />
         </Interactable.View>
 
@@ -89,12 +98,24 @@ const styles = StyleSheet.create({
     marginTop: 20
   },
   background: {
-    backgroundColor: '#000000',
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     bottom: 0,
     left: 0,
     right: 0
   },
-  list: {}
+  interactable: {
+    flex: 1,
+    flexDirection: "column"
+  },
+  listHandle: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 70,
+    backgroundColor: "#efefef"
+  },
+  list: {
+    flex: 1,
+    height: Screen.height
+  }
 });
